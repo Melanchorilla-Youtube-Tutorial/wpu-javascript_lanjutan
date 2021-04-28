3c38cf5e14c7a33f22038c7fe8f320916767c293
melanchorilla <dickykamaludin12@gmail.com>
2021-04-29 05:11:54 +0700

    12 13 fix

---
+++ 13 Fetch (Refactor)/script.js
@@ -0,0 +1,116 @@
+// const searchButton = document.querySelector('.search-button');
+// searchButton.addEventListener('click', function() {
+
+//     const inputKeyword = document.querySelector('.input-keyword');
+
+//     fetch('http://www.omdbapi.com/?apikey=97ed753d&s=' + inputKeyword.value)
+//         .then(response => response.json())
+//         .then(response => {
+//             const movies = response.Search;
+//             let cards = '';
+//             movies.forEach(m => cards += showCards(m));
+
+//             const movieContainer = document.querySelector('.movie-container');
+//             movieContainer.innerHTML = cards;
+
+//             // ketika tombol detail diklik
+//             const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+//             modalDetailButton.forEach(btn => {
+//                 btn.addEventListener('click', function(){
+//                     const imdbid = this.dataset.imdbid;
+                    
+//                     fetch('http://www.omdbapi.com/?apikey=97ed753d&i=' + imdbid)
+//                         .then(response => response.json())
+//                         .then(m => {
+//                             const movieDetail = showMovieDetail(m);
+//                             const modalBody = document.querySelector('.modal-body');
+//                             modalBody.innerHTML = movieDetail;
+//                         })
+                    
+//                 });
+//             });
+
+//         });
+
+// });
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', async function() {
+    const inputKeyword = document.querySelector('.input-keyword');
+
+    const movies = await getMovies(inputKeyword.value);
+    // console.log(movies);
+    updateUI(movies);
+});
+
+
+// event binding
+document.addEventListener('click', async function(e) {
+    if(e.target.classList.contains('modal-detail-button')) {
+        const imdbid = e.target.dataset.imdbid;
+
+        const movieDetail = await getMovieDetail(imdbid);
+        updateUIDetail(movieDetail);
+
+    }
+})
+
+
+function getMovieDetail(imdbid) {
+    return fetch('http://www.omdbapi.com/?apikey=97ed753d&i=' + imdbid)
+        .then(response => response.json())
+        .then(m => m);
+}
+
+function updateUIDetail(m) {
+    const movieDetail = showMovieDetail(m);
+    const modalBody = document.querySelector('.modal-body');
+    modalBody.innerHTML = movieDetail;
+}
+
+function getMovies(keyword){
+    return fetch('http://www.omdbapi.com/?apikey=97ed753d&s=' + keyword)
+        .then(response => response.json())
+        .then(response => response.Search);
+}
+
+function updateUI(movies){
+    // const movies = response.Search;
+    let cards = '';
+    movies.forEach(m => cards += showCards(m));
+
+    const movieContainer = document.querySelector('.movie-container');
+    movieContainer.innerHTML = cards;
+}
+
+function showCards(movie){
+    return `<div class="col-md-4 my-3">
+                <div class="card">
+                <img class="card-img-top" src="${movie.Poster}">
+                <div class="card-body">
+                    <h5 class="card-title">${movie.Title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">2016</h6>
+                    <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${movie.imdbID}">Show Details</a>
+                </div>
+                </div>
+            </div>`;
+}
+
+function showMovieDetail(m) {
+    return `<div class="container-fluid">
+                <div class="row">
+                <div class="col-md-3">
+                    <img src="${m.Poster}" class="img-fluid">
+                </div>
+                <div class="col-md">
+                    <ul class="list-group">
+                    <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                    <li class="list-group-item"><strong>Director: </strong>${m.Director}</li>
+                    <li class="list-group-item"><strong>Actors: </strong>${m.Actors}</li>
+                    <li class="list-group-item"><strong>Writer: </strong>${m.Writer}</li>
+                    <li class="list-group-item"><strong>Plot: </strong>${m.Plot}</li>
+                    </ul>
+                </div>
+                </div>
+            </div>`;
+}
